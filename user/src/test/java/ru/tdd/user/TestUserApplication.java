479fd514f5633dac66258b06cf4e0a4bd536698a
danil <danil.tribushko@bk.ru>
2026-01-29 17:06:15 +0500

    29.01.2026 - Создал модель user

---
+++ user/src/test/java/ru/tdd/user/TestUserApplication.java
@@ -0,0 +1,11 @@
+package ru.tdd.user;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(UserApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
